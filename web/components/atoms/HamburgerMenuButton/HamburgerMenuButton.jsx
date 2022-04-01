0cfb96c845e6d9b366ec2851ec8327e734cc1283
dmrf1980 <mrodriguez1899@gmail.com>
2022-03-31 22:39:18 -0300

    Correcciones en el componente HamburgerMenuButton

---
+++ web/components/atoms/HamburgerMenuButton/HamburgerMenuButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class HamburgerMenuButton extends React.Component {
+  render() {
+    return <button className="hamburgerMenuButton"></button>;
+  }
+}
+
+export default HamburgerMenuButton;
